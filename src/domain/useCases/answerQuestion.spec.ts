758c8bbc80562ce8a53a9efaa7b0b07e0f1d333b
Ruan Brito <ruan.desenvolvedor@gmail.com>
2024-10-29 14:35:32 -0300

    refactor: removed vitest imports and updated style by eslint

--- src/domain/useCases/answerQuestion.spec.ts
+++ src/domain/useCases/answerQuestion.spec.ts
@@ -1,21 +1,19 @@
-import { expect, test } from 'vitest'
 import { AnswerQuestionUseCase } from './answerQuestion'
 import { IAnswerRepository } from '../repositories/contracts/answersRespository'
 import { Answer } from '../entities/answer'
 
 const fakeAnswerRepository: IAnswerRepository = {
-  create: async (answer: Answer) => {
-    return
-  }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  create: async (answer: Answer) => {},
 }
 
 test('create an answer', async () => {
   const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
 
-  const answer =  await answerQuestion.execute({ 
+  const answer = await answerQuestion.execute({
     questionId: '1',
     instructorId: '1',
-    content: 'Nova Resposta'
+    content: 'Nova Resposta',
   })
 
   expect(answer.content).toEqual('Nova Resposta')
--- src/domain/useCases/answerQuestion.ts
+++ src/domain/useCases/answerQuestion.ts
@@ -1,6 +1,6 @@
-import { UniqueEntityId } from "../../core/entities/uniqueEntityId"
-import { Answer } from "../entities/answer"
-import { IAnswerRepository } from "../repositories/contracts/answersRespository"
+import { UniqueEntityId } from '@/core/entities/uniqueEntityId'
+import { IAnswerRepository } from '../repositories/contracts/answersRespository'
+import { Answer } from '../entities/answer'
 
 interface AnswerQuestionUseCaseRequest {
   instructorId: string
@@ -9,19 +9,18 @@ interface AnswerQuestionUseCaseRequest {
 }
 
 export class AnswerQuestionUseCase {
-  constructor(
-    private answerRepository: IAnswerRepository
-  ){
+  constructor(private answerRepository: IAnswerRepository) {}
 
-  }
-
-   async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+  async execute({
+    instructorId,
+    questionId,
+    content,
+  }: AnswerQuestionUseCaseRequest) {
     const answer = Answer.create({
       authorId: new UniqueEntityId(instructorId),
       questionId: new UniqueEntityId(questionId),
       content,
     })
-
 
     await this.answerRepository.create(answer)
 
