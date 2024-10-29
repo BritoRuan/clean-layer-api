7049393e2f279db81736c3d8c7eb8968873b36c5
Ruan Brito <ruan.desenvolvedor@gmail.com>
2024-10-29 18:04:12 -0300

    feat: added question tests use case

---
+++ src/domain/forum/application/useCases/answerQuestionUseCase/answerQuestion.spec.ts
@@ -0,0 +1,23 @@
+import { InMemoryAnswerRepository } from 'test/repositories/InMemoryRepository/InMemoryAnswersRepository/InMemoryAnswersRepository'
+import { AnswerQuestionUseCase } from './answerQuestion'
+
+let inMemoryAnswersRepository: InMemoryAnswerRepository
+let sut: AnswerQuestionUseCase
+
+describe('Create Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswerRepository()
+    sut = new AnswerQuestionUseCase(inMemoryAnswersRepository)
+  })
+
+  it('should be able to create a question', async () => {
+    const { answer } = await sut.execute({
+      questionId: '1',
+      instructorId: '1',
+      content: 'Conteúdo da resposta',
+    })
+
+    expect(answer.id).toBeTruthy()
+    expect(inMemoryAnswersRepository.items[0].id).toEqual(answer.id)
+  })
+})
