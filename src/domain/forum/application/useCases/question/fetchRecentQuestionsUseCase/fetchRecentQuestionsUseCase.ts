f5146badb7699ba0bfb7919739d9923275a7e867
Ruan Brito <ruan.desenvolvedor@gmail.com>
2024-11-04 10:23:55 -0300

    feat: added fetch recent questions use case

---
+++ src/domain/forum/application/useCases/question/fetchRecentQuestionsUseCase/fetchRecentQuestionsUseCase.ts
@@ -0,0 +1,24 @@
+import { Question } from '@/domain/forum/enterprise/entities/question'
+import { IQuestionRepository } from '../../../repositories/contracts/questionRepository'
+
+interface FetchRecentQuestionsUseCaseRequest {
+  page: number
+}
+
+interface FetchRecentQuestionsUseCaseResponse {
+  questions: Question[]
+}
+
+export class FetchRecentQuestionsUseCase {
+  constructor(private questionRepository: IQuestionRepository) {}
+
+  async execute({
+    page,
+  }: FetchRecentQuestionsUseCaseRequest): Promise<FetchRecentQuestionsUseCaseResponse> {
+    const questions = await this.questionRepository.findManyRecent({ page })
+
+    return {
+      questions,
+    }
+  }
+}
