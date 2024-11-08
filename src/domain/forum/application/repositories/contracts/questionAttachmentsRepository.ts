3984a34ae4306ddd313752e2a6a738357aacecc9
Ruan Brito <ruan.desenvolvedor@gmail.com>
2024-11-07 21:37:06 -0300

    feat: added question attachments repository interface

---
+++ src/domain/forum/application/repositories/contracts/questionAttachmentsRepository.ts
@@ -0,0 +1,5 @@
+import { QuestionAttachment } from '@/domain/forum/enterprise/entities/questionAttachment'
+
+export interface IQuestionAttachmentsRepository {
+  findManyByQuestionId(questionId: string): Promise<QuestionAttachment[]>
+}
