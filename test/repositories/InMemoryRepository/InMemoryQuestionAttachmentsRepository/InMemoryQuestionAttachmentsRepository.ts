513fb8d860feb03d277e72848ae99a65b8d56e50
Ruan Brito <ruan.desenvolvedor@gmail.com>
2024-11-07 21:35:12 -0300

    feat: added in-memory question attachment repository

---
+++ test/repositories/InMemoryRepository/InMemoryQuestionAttachmentsRepository/InMemoryQuestionAttachmentsRepository.ts
@@ -0,0 +1,16 @@
+import { IQuestionAttachmentsRepository } from '@/domain/forum/application/repositories/contracts/questionAttachmentsRepository'
+import { QuestionAttachment } from '@/domain/forum/enterprise/entities/questionAttachment'
+
+export class InMemoryQuestionAttachmentRepository
+  implements IQuestionAttachmentsRepository
+{
+  public items: QuestionAttachment[] = []
+
+  async findManyByQuestionId(questionId: string) {
+    const questionAttachments = this.items.filter(
+      (item) => item.questionId.toString() === questionId,
+    )
+
+    return questionAttachments
+  }
+}
